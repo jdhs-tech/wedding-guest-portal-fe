5ef18f7c04f2ac3776159a9e33eeaf27e973cc3d
Sadeesha Perera <121984253+SadeeshaPerera@users.noreply.github.com>
2025-07-09 08:44:42 +0000

    Implement code changes to enhance functionality

---
+++ craco.config.js
@@ -0,0 +1,31 @@
+const { ModuleFederationPlugin } = require("webpack").container;
+
+module.exports = {
+  devServer: {
+    port: 3002,
+  },
+  webpack: {
+    plugins: {
+      add: [
+        new ModuleFederationPlugin({
+          name: "AnotherModule",
+          exposes: {
+            "./SampleComponent": "./src/shared/SampleComponent",
+          },
+          filename: "remoteEntry.js",
+          shared: {
+            react: { singleton: true },
+            "react-dom": { singleton: true },
+          },
+        }),
+      ],
+    },
+    configure: (webpackConfig) => ({
+      ...webpackConfig,
+      output: {
+        ...webpackConfig.output,
+        publicPath: "auto",
+      },
+    }),
+  },
+};
